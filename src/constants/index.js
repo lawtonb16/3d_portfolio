8fec266a1b27242c5df28dd61001c10423d18e43
Benjamin Lawton <106443433+lawtonb16@users.noreply.github.com>
2023-04-25 11:14:54 -0500

    updated index

--- src/constants/index.js
+++ src/constants/index.js
@@ -245,7 +245,7 @@ const projects = [
             },
         ],
         image: tripguide,
-        source_code_link: "https://github.com/",
+        source_code_link: "https://github.com/lawtonb16/travel_advisor",
     },
 ];
 
